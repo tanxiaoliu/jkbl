7ee5fc86cafb5941bc5c9c13d519fb0f88a0798c
tanhuaxin <tanhauxin@gmail.com>
2016-12-22 18:53:46 +0800

    修改用户登录

--- application/Portal/Controller/IndexController.class.php
+++ application/Portal/Controller/IndexController.class.php
@@ -30,21 +30,31 @@ use Wechat;
 /**
  * 首页
  */
-class IndexController extends HomebaseController {
+class IndexController extends HomebaseController
+{
 
 	protected $weObj;
-	protected $url;
+	protected $userInfo;
+	const TOKEN = 'test';
+	const APPID = 'wx33d9402ea60d3681';
+	const APPSECRET = 'eb34a1662269b9027b7ed8635b04c6ed';
+	const API_BASE_URL_PREFIX = 'https://api.weixin.qq.com';
+	const URL = 'http://laoit.top';
+
 	public function __construct()
 	{
-		parent::__construct ();
+		parent::__construct();
 	}
 
-	//首页 小夏是老猫除外最帅的男人了
+	/**
+	 * 检测用户是否登录
+	 * @author tanhuaxin
+	 */
 	public function index()
 	{
 		if (sp_is_weixin()) {
-			$userInfo = S('userInfo');
-			if($userInfo == false && isset($userInfo->openid)){
+			$userInfo = json_decode($_COOKIE['userInfo']);
+			if (empty($userInfo) || !isset($userInfo->openid)) {
 				//微信登录
 				$options = array(
 					'token' => 'test', //填写你设定的key
@@ -52,47 +62,65 @@ class IndexController extends HomebaseController {
 					'appsecret' => 'eb34a1662269b9027b7ed8635b04c6ed' //填写高级调用功能的密钥
 				);
 				$this->weObj = new Wechat($options);
-				$this->url = $this->weObj->getOauthRedirect('http://laoit.top/index.php?g=portal&m=index&a=test', '', 'snsapi_base');
-				redirect($this->url);
-			} else { 
-				$this->assign("userInfo", $userInfo);
-				$this->display(":index");
+				redirect($this->weObj->getOauthRedirect(self::URL . U('callback')));
+			} else {
+				redirect(U('home'));
 			}
 		} else {
 			//提示请使用微信登录
-			die ( '这是微信请求的接口地址，直接在浏览器里无效' );
+			die ('这是微信请求的接口地址，直接在浏览器里无效');
 		}
 	}
 
-	function test() {
-        $code = $_GET['code'];
-		$url1 = 'https://api.weixin.qq.com/sns/oauth2/access_token?appid=wx33d9402ea60d3681&secret=eb34a1662269b9027b7ed8635b04c6ed&code='.$code.'&grant_type=authorization_code';
-		$result =  json_decode(file_get_contents($url1));
-		$url2 = 'https://api.weixin.qq.com/sns/userinfo?access_token='.$result->access_token.'&openid='.$result->openid.'&lang=zh_CN';
-		$userInfo =  json_decode(file_get_contents($url2));
-		$map['user_login'] = $userInfo->openid;
-		$users = D('users')->where($map)->find();
-		if($users){
-			$data['last_login_time'] = date("Y-m-d H:i:s",time());
-			D('users')->save($data);
+	/**
+	 * code回调页面
+	 * @author tanhuaxin
+	 */
+	function callback()
+	{
+		$code = $_GET['code'];
+		$url = self::API_BASE_URL_PREFIX . '/sns/oauth2/access_token?appid=' . self::APPID . '&secret=' . self::APPSECRET . '&code=' . $code . '&grant_type=authorization_code';
+		$result = json_decode(file_get_contents($url));
+		$url2 = self::API_BASE_URL_PREFIX . '/sns/userinfo?access_token=' . $result->access_token . '&openid=' . $result->openid . '&lang=zh_CN';
+		$result2 = file_get_contents($url2);
+		$userInfo = json_decode($result2);
+		if (!empty($userInfo) || isset($userInfo->openid)) {
+			//保存用户信息入库
+			$map['user_login'] = $userInfo->openid;
+			$users = D('users')->where($map)->find();
+			if ($users) {
+				$data['last_login_time'] = date("Y-m-d H:i:s", time());
+				D('users')->save($data);
+			} else {
+				$data['user_login'] = $userInfo->openid;
+				$data['user_pass'] = sp_password('123456');
+				$data['user_nicename'] = $userInfo->nickname;
+				$data['avatar'] = $userInfo->headimgurl;
+				$data['sex'] = $userInfo->sex;
+				$data['last_login_time'] = date("Y-m-d H:i:s", time());
+				$data['create_time'] = date("Y-m-d H:i:s", time());
+				$data['user_type'] = 2;
+				D('users')->add($data);
+			}
+			setcookie('userInfo', $result2);
+			redirect(U('home'));
 		} else {
-			$data['user_login'] = $userInfo->openid;
-			$data['user_pass'] = sp_password('123456');
-			$data['user_nicename'] = $userInfo->nickname;
-			$data['avatar'] = $userInfo->headimgurl;
-			$data['sex'] = $userInfo->sex;
-			$data['last_login_time'] = date("Y-m-d H:i:s",time());
-			$data['create_time'] = date("Y-m-d H:i:s",time());
-			$data['user_type'] = 2;
-			D('users')->add($data);
+			die ('获取用户信息失败，请联系管理员');
 		}
-		S('userInfo', $userInfo);
-		redirect('http://laoit.top');
 	}
+
+	//首页 小强是爸爸除外最帅的男人了
+	public function home()
+	{
+		$userInfo = $_COOKIE['userInfo'];
+		$this->assign("userInfo", json_decode($userInfo));
+		$this->display(":index");
+	}
+
 	/**
 	 * 联盟
 	 * @author tanhuaxin
-     */
+	 */
 	public function member()
 	{
 		$this->display(":member");
