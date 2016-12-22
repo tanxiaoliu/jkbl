c8a8f37810e14d82b9f933dfac670ddc326abd3a
tanhuaxin <tanhauxin@gmail.com>
2016-12-22 14:45:48 +0800

    获取用户信息

--- application/Portal/Controller/IndexController.class.php
+++ application/Portal/Controller/IndexController.class.php
@@ -24,50 +24,56 @@
 // | Author: Dean <zxxjjforever@163.com>
 // +----------------------------------------------------------------------
 namespace Portal\Controller;
-use Common\Controller\HomebaseController; 
+use Common\Controller\HomebaseController;
+use Wechat;
+
 /**
  * 首页
  */
 class IndexController extends HomebaseController {
-	
-    //首页 小夏是老猫除外最帅的男人了
+
+	protected $weObj;
+	protected $url;
+	protected $userInfo;
+	public function __construct()
+	{
+		parent::__construct ();
+	}
+
+	//首页 小夏是老猫除外最帅的男人了
 	public function index()
 	{
 		if (sp_is_weixin()) {
-			//微信登录
-			$options = array(
-				'token' => 'tokenaccesskey', //填写你设定的key
-				'encodingaeskey' => 'encodingaeskey', //填写加密用的EncodingAESKey
-				'appid' => 'wxdk1234567890', //填写高级调用功能的app id
-				'appsecret' => 'xxxxxxxxxxxxxxxxxxx' //填写高级调用功能的密钥
-			);
-			$weObj = new \Wechat($options);
-			$weObj->valid();
-			$result = $weObj->getOauthRedirect($weObj->getOauthAccessToken());
-			$UserInfo = $weObj->getOauthUserinfo($result['access_token'],$result['openid']);//获取授权后的用户资料
-			var_dump($UserInfo);exit;
-			$type = $weObj->getRev()->getRevType();
-			switch ($type) {
-				case \Wechat::MSGTYPE_TEXT:
-					$weObj->text("hello, I'm wechat")->reply();
-					exit;
-					break;
-				case \Wechat::MSGTYPE_EVENT:
-					//....
-					break;
-				case \Wechat::MSGTYPE_IMAGE:
-					//...
-					break;
-				default:
-					$weObj->text("help info")->reply();
+			$this->userInfo = S('userInfo');
+			if($this->userInfo == false){
+				//微信登录
+				$options = array(
+					'token' => 'test', //填写你设定的key
+					'appid' => 'wx33d9402ea60d3681', //填写高级调用功能的app id
+					'appsecret' => 'eb34a1662269b9027b7ed8635b04c6ed' //填写高级调用功能的密钥
+				);
+				$this->weObj = new Wechat($options);
+				$this->url = $this->weObj->getOauthRedirect('http://laoit.top/index.php?g=portal&m=index&a=test', '', 'snsapi_base');
+				redirect($this->url);
+			} else {
+				$this->assign("userInfo", $this->userInfo);
+				$this->display(":index");
 			}
-
 		} else {
 			//提示请使用微信登录
+			die ( '这是微信请求的接口地址，直接在浏览器里无效' );
 		}
-		$this->display(":index");
 	}
 
+	function test() {
+        $code = $_GET['code'];
+		$url = 'https://api.weixin.qq.com/sns/oauth2/access_token?appid=wx33d9402ea60d3681&secret=eb34a1662269b9027b7ed8635b04c6ed&code='.$code.'&grant_type=authorization_code';
+		$result =  json_decode(file_get_contents($url));
+		$url2 = 'https://api.weixin.qq.com/sns/userinfo?access_token='.$result->access_token.'&openid='.$result->openid.'&lang=zh_CN';
+		$this->userInfo =  json_decode(file_get_contents($url2));
+		S('userInfo', $this->userInfo);
+		redirect('http://laoit.top');
+	}
 	/**
 	 * 联盟
 	 * @author tanhuaxin
