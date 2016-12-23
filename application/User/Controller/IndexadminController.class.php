87f8e5227983fc8f7a3ce9cd400d6a7d6c94d0ef
liqiang <455019211@qq.com>
2016-12-23 21:58:00 +0800

    后台新增分组以及页面优化

--- application/User/Controller/IndexadminController.class.php
+++ application/User/Controller/IndexadminController.class.php
@@ -24,6 +24,41 @@ class IndexadminController extends AdminbaseController {
             $where['_complex'] = $keyword_complex;
         }
         
+        $users_model=M("Users");
+        
+        $count=$users_model->where($where)->count();
+        $page = $this->page($count, 20);
+        
+        $list = $users_model
+        ->where($where)
+        ->order("create_time DESC")
+        ->limit($page->firstRow . ',' . $page->listRows)
+        ->select();
+        
+        $this->assign('list', $list);
+        $this->assign("page", $page->show('Admin'));
+        
+        $this->display(":index");
+    }
+   // 后台本站用户列表
+    public function group(){
+        $where=array();
+        $request=I('request.');
+        
+        if(!empty($request['uid'])){
+            $where['id']=intval($request['uid']);
+        }
+        
+        if(!empty($request['keyword'])){
+            $keyword=$request['keyword'];
+            $keyword_complex=array();
+            $keyword_complex['user_login']  = array('like', "%$keyword%");
+            $keyword_complex['user_nicename']  = array('like',"%$keyword%");
+            $keyword_complex['user_email']  = array('like',"%$keyword%");
+            $keyword_complex['_logic'] = 'or';
+            $where['_complex'] = $keyword_complex;
+        }
+        
     	$users_model=M("Users");
     	
     	$count=$users_model->where($where)->count();
