a9054c21ea976855a6fe0ecdfb8d8610dbfbc3cd
tanxiaoliu <tanxiaoliu@qq.com>
2016-12-25 17:20:11 +0800

    新增数据ex导出

--- application/Admin/Controller/SportController.class.php
+++ application/Admin/Controller/SportController.class.php
@@ -5,76 +5,86 @@ use Common\Controller\AdminbaseController;
 use PHPExcel_Cell;
 use PHPExcel_IOFactory;
 
-class SportController extends AdminbaseController{
-    
-	protected $sport_model;
-	
-	public function _initialize() {
-		parent::_initialize();
-		$this->sport_model = M("SportRecord");
-	}
-	public function index(){
-		$where = array();
-		$count=$this->sport_model->count();
-		$page = $this->page($count, 20);
+class SportController extends AdminbaseController
+{
+
+    protected $sport_model;
+
+    public function _initialize()
+    {
+        parent::_initialize();
+        $this->sport_model = M("SportRecord");
+    }
+
+    public function index()
+    {
+        $where = array();
+        $count = $this->sport_model->count();
+        $page = $this->page($count, 20);
         $sport = $this->sport_model
             ->where($where)
             ->order("add_time DESC")
             ->limit($page->firstRow, $page->listRows)
             ->select();
-		$this->assign("page",$page->show('Admin'));
-		$this->assign("sport",$sport);
-		$this->display();
-	}
-	// 物品添加或编辑
-	public function add() {
-		$data =array();
-		if(!empty($_POST)){
-			$data=I("post.");
-			$data['nums'] = 0;
-			$data['add_time'] = time();
-			if ($this->sport_model->create($data)!==false) {
-				if (isset($data['id'])) {
-					if ($this->sport_model->save()!==false) {
-						$this->success("保存成功！", U("sport/index"));
-					} else {
-						$this->error("保存失败！");
-					}
-				}else{
-					if ($this->sport_model->add()!==false) {
-						$this->success("添加成功！", U("sport/index"));
-					} else {
-						$this->error("添加失败！");
-					}
-				}
-			} else {
-				$this->error($this->sport_model->getError());
-			}
-		}else{
-			if (isset($_GET['id'])&&!empty($_GET['id'])) {
-				$id = I("get.id",0,'intval');
-				$data   =  $this->sport_model->find($id);
-			}
-			if (!empty($data)) {
-				$this->assign('data',$data);
-		    	$this->display(":Sport/edit");
-			}
-			else{
-				$this->display();
-			}
-		}
-		
-	}
-	// 删除导航分类
-	public function delete(){
-		$id = I("get.id",0,'intval');
-		if ($this->sport_model->where(array('id'=>$id))->delete()!==false) {
-			$this->success("删除成功！");
-		} else {
-			$this->error("删除失败！");
-		}
-	}
+        $this->assign("page", $page->show('Admin'));
+        $this->assign("sport", $sport);
+        $this->display();
+    }
+
     // 物品添加或编辑
+    public function add()
+    {
+        $data = array();
+        if (!empty($_POST)) {
+            $data = I("post.");
+            $data['nums'] = 0;
+            $data['add_time'] = time();
+            if ($this->sport_model->create($data) !== false) {
+                if (isset($data['id'])) {
+                    if ($this->sport_model->save() !== false) {
+                        $this->success("保存成功！", U("sport/index"));
+                    } else {
+                        $this->error("保存失败！");
+                    }
+                } else {
+                    if ($this->sport_model->add() !== false) {
+                        $this->success("添加成功！", U("sport/index"));
+                    } else {
+                        $this->error("添加失败！");
+                    }
+                }
+            } else {
+                $this->error($this->sport_model->getError());
+            }
+        } else {
+            if (isset($_GET['id']) && !empty($_GET['id'])) {
+                $id = I("get.id", 0, 'intval');
+                $data = $this->sport_model->find($id);
+            }
+            if (!empty($data)) {
+                $this->assign('data', $data);
+                $this->display(":Sport/edit");
+            } else {
+                $this->display();
+            }
+        }
+
+    }
+
+    // 删除导航分类
+    public function delete()
+    {
+        $id = I("get.id", 0, 'intval');
+        if ($this->sport_model->where(array('id' => $id))->delete() !== false) {
+            $this->success("删除成功！");
+        } else {
+            $this->error("删除失败！");
+        }
+    }
+
+    /**
+     * 导入数据
+     */
     public function importData()
     {
         if (!empty ($_FILES ['file_stu'] ['name'])) {
@@ -107,40 +117,52 @@ class SportController extends AdminbaseController{
             /*对生成的数组进行数据库的写入*/
             foreach ($res as $k => $v) {
                 if ($k != 1) {
-                    //添加运动记录
-                    $data ['openid'] = $v [0];
-                    $data ['nick_name'] = $v [1];
-                    $data ['add_time'] = time();
-                    $data ['step_nums'] = $v [2];
-                    $result = M('SportRecord')->add($data);
-                    if (!$result) {
-                        $this->error('导入数据库失败');
+                    if ($v[2] != 0) {
+                        //添加运动记录
+                        $data ['openid'] = $v[0];
+                        $data ['nick_name'] = $v[1];
+                        $data ['add_time'] = time();
+                        $data ['step_nums'] = $v[2];
+                        $result = M('SportRecord')->add($data);
+                        if (!$result) {
+                            $this->error('导入数据库失败');
+                        }
+                        //添加腾币记录
+                        $coinData ['openid'] = $v[0];
+                        $coinData ['type'] = 1;
+                        $coinData ['add_time'] = time();
+                        $coinData ['coin'] = $v[2];
+                        $coinData ['type_id'] = $result;
+                        $coinResult = M('CoinRecord')->add($coinData);
+                        if (!$coinResult) {
+                            $this->error('导入数据库失败');
+                        }
+                        //更新用户的总腾币
+                        $map['user_login'] = $v[0];
+                        M('Users')->where($map)->setInc('score', $v[2]);
                     }
-                    //添加腾币记录
-                    $coinData ['openid'] = $v [0];
-                    $coinData ['type'] = 1;
-                    $coinData ['add_time'] = time();
-                    $coinData ['coin'] = $v [2];
-                    $coinData ['type_id'] = $result;
-                    $coinResult = M('CoinRecord')->add($coinData);
-                    if (!$coinResult) {
-                        $this->error('导入数据库失败');
-                    }
-                    //更新用户的总腾币
-                    $map['user_login'] = $v [0];
-                    M('Users')->where($map)->setInc('score', $v [2]);
                 }
             }
             $this->success('导入数据成功', U("sport/index"));
             exit();
         } else {
-            $this->error('导入数据失败4', U("sport/index"));
+            $this->error('导入数据失败', U("sport/index"));
             exit();
         }
     }
 
-    public function read($filename, $encode='utf-8'){
-        include_once(SITE_PATH .'/simplewind/Lib/Util/PHPExcel.php');
+    /**
+     * 导出Excel
+     */
+    public function pushExcel()
+    {
+        $data = D('users')->field('user_login as openid, user_nicename as nick_name')->select();
+        $this->push($data, '运动记录' . date('Ymd', time()));
+    }
+
+    public function read($filename, $encode = 'utf-8')
+    {
+        include_once(SITE_PATH . '/simplewind/Lib/Util/PHPExcel.php');
         $objReader = PHPExcel_IOFactory::createReader('Excel5');
         $objReader->setReadDataOnly(true);
         $objPHPExcel = $objReader->load($filename);
@@ -151,11 +173,45 @@ class SportController extends AdminbaseController{
         $excelData = array();
         for ($row = 1; $row <= $highestRow; $row++) {
             for ($col = 0; $col < $highestColumnIndex; $col++) {
-                $excelData[$row][] =(string)$objWorksheet->getCellByColumnAndRow($col, $row)->getValue();
+                $excelData[$row][] = (string)$objWorksheet->getCellByColumnAndRow($col, $row)->getValue();
             }
         }
         return $excelData;
     }
 
-
+    public function push($data, $name = 'Excel')
+    {
+        include_once(SITE_PATH . '/simplewind/Lib/Util/PHPExcel.php');
+        error_reporting(E_ALL);
+        date_default_timezone_set('Europe/London');
+        $objPHPExcel = new \PHPExcel();
+        /*以下是一些设置 ，什么作者  标题啊之类的*/
+        $objPHPExcel->getProperties()->setCreator("转弯的阳光")
+            ->setLastModifiedBy("转弯的阳光")
+            ->setTitle("数据EXCEL导出")
+            ->setSubject("数据EXCEL导出")
+            ->setDescription("备份数据")
+            ->setKeywords("excel")
+            ->setCategory("result file");
+        /*以下就是对处理Excel里的数据， 横着取数据，主要是这一步，其他基本都不要改*/
+        $objPHPExcel->setActiveSheetIndex(0)
+            ->setCellValue('A1', 'openid')
+            ->setCellValue('B1', 'nick_name')
+            ->setCellValue('C1', 'step_nums');
+        foreach ($data as $k => $v) {
+            $num = $k + 2;
+            $objPHPExcel->setActiveSheetIndex(0)
+                //Excel的第A列，uid是你查出数组的键值，下面以此类推
+                ->setCellValue('A' . $num, $v['openid'])
+                ->setCellValue('B' . $num, $v['nick_name'])
+                ->setCellValue('C' . $num, 0);
+        }
+        $objPHPExcel->setActiveSheetIndex(0);
+        header('Content-Type: application/vnd.ms-excel');
+        header('Content-Disposition: attachment;filename="' . $name . '.xls"');
+        header('Cache-Control: max-age=0');
+        $objWriter = PHPExcel_IOFactory::createWriter($objPHPExcel, 'Excel5');
+        $objWriter->save('php://output');
+        exit;
+    }
 }
